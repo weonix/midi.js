88de6833faa75b431c1ef126573a9b976d9f6089
weonix <31888635+weonix@users.noreply.github.com>
2023-05-31 18:27:36 +0800

    Fixed loadplugins not waiting for proper timing

--- src/loader.js
+++ src/loader.js
@@ -41,68 +41,69 @@ root.loadPlugin = async function (opts) {
   root.soundfontUrl = opts.soundfontUrl || root.soundfontUrl
 
   // / Detect the best type of audio to use
-  audioDetect( async function (supports) {
-    var hash = window.location.hash
-    var api = ''
-    var success = false;
+  await new Promise(resolve => {
+    audioDetect( async function (supports) {
+      var hash = window.location.hash
+      var api = ''
+      var success = false;
 
-    //console.log(supports);
+      //console.log(supports);
 
-    // / use the most appropriate plugin if not specified
-    // if (supports[opts.api]) {
-    //   api = opts.api
-    // } 
-    // if (supports[hash.substr(1)]) {
-    //   api = hash.substr(1)
-    // } 
-    // if (supports.webmidi) {
-    //   api = 'webmidi'
-    // } 
-    // if (window.AudioContext) { // Chrome
-    //   api = 'webaudio'
-    // } 
-    // if (window.Audio) { // Firefox
-    //   api = 'audiotag'
+      // / use the most appropriate plugin if not specified
+      // if (supports[opts.api]) {
+      //   api = opts.api
+      // } 
+      // if (supports[hash.substr(1)]) {
+      //   api = hash.substr(1)
+      // } 
+      // if (supports.webmidi) {
+      //   api = 'webmidi'
+      // } 
+      // if (window.AudioContext) { // Chrome
+      //   api = 'webaudio'
+      // } 
+      // if (window.Audio) { // Firefox
+      //   api = 'audiotag'
+      // }
+
+    // if (connect[api]) {
+        // / use audio/ogg when supported
+      let audioFormat
+      if (opts.targetFormat) {
+        audioFormat = opts.targetFormat
+      } else { // use best quality
+        audioFormat = supports['audio/ogg'] ? 'ogg' : 'mp3'
+      }
+
+      // / load the specified plugin
+      root.__api = api
+      root.__audioFormat = audioFormat
+      root.supports = supports
+      root.loadResource(opts)
     // }
 
-    console.log("detect");
+      //connect[root.__api](opts)
+      var list = [];
 
-   // if (connect[api]) {
-      // / use audio/ogg when supported
-    let audioFormat
-    if (opts.targetFormat) {
-      audioFormat = opts.targetFormat
-    } else { // use best quality
-      audioFormat = supports['audio/ogg'] ? 'ogg' : 'mp3'
-    }
+      if (supports.webmidi) {
+        api = 'webmidi'
+        list.push(connect['webmidi'](opts));
+      } 
+      if (window.AudioContext) { // Chrome
+        api = 'webaudio'
+        list.push(connect['webaudio'](opts));
+      } 
+      // if (window.Audio) { // Firefox
+      //   api = 'audiotag'
+      //   list.push(connect['audiotag'](opts));
+      // }
 
-    // / load the specified plugin
-    root.__api = api
-    root.__audioFormat = audioFormat
-    root.supports = supports
-    root.loadResource(opts)
-   // }
-
-    //connect[root.__api](opts)
-    var list = [];
-
-    if (supports.webmidi) {
-      api = 'webmidi'
-      list.push(connect['webmidi'](opts));
-    } 
-    if (window.AudioContext) { // Chrome
-      api = 'webaudio'
-      list.push(connect['webaudio'](opts));
-    } 
-    // if (window.Audio) { // Firefox
-    //   api = 'audiotag'
-    //   list.push(connect['audiotag'](opts));
-    // }
-
-    for (const item of list) {
-      await item;
-    }
-  })
+      for (const item of list) {
+        await item;
+      }
+      resolve();
+    })
+  });
 }
 
 /*
@@ -140,23 +141,27 @@ root.loadResource = function (opts) {
 }
 
 var connect = {
-  webmidi: function (opts) {
+  webmidi: async function (opts) {
     // cant wait for this to be standardized!
-    root.WebMIDI.connect(opts)
+    await root.WebMIDI.connect(opts)
   },
   audiotag: function (opts) {
     // works ok, kinda like a drunken tuna fish, across the board
     // http://caniuse.com/audio
     requestQueue(opts, 'AudioTag')
   },
-  webaudio: function (opts) {
+  webaudio: async function (opts) {
     // works awesome! safari, chrome and firefox support
     // http://caniuse.com/web-audio
-    requestQueue(opts, 'WebAudio')
+    
+     await new Promise((resolve, reject)=>{
+        requestQueue(opts, 'WebAudio', resolve)
+     });
+
   }
 }
 
-var requestQueue = function (opts, context) {
+var requestQueue = function (opts, context, onFinish) {
   var audioFormat = opts.format
   var instruments = opts.instruments
   var onprogress = opts.onprogress
@@ -164,10 +169,13 @@ var requestQueue = function (opts, context) {
   // /
   var length = instruments.length
   var pending = length
-  var waitForEnd = function () {
+  var waitForEnd = async function () {
     if (!--pending) {
       onprogress && onprogress('load', 1.0)
-      root[context].connect(opts)
+      //console.log("waitForEnd1");
+      await root[context].connect(opts)
+      //console.log("waitForEnd2");
+      onFinish();
     }
   }
   // /
